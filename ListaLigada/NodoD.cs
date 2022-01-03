46dc176e4fe63678a2fb375ecb0856a2a039ce3f
Jumara Fernandes <jumaraandrade4@gmail.com>
2022-01-03 19:57:38 +0000

    Organizando os ficheiros
    Co-Authored-By: Redney Monteiro <79022289+RedneyMonteiro15@users.noreply.github.com>

---
+++ ListaLigada/NodoD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+ 
+namespace Trabalho
+{
+/// <summary>
+/// Classe NODO utilizada para guardar as informações
+/// </summary>
+   [Serializable]
+   public class NodoD<K>
+	{
+   	
+     // auto implemented propriedades...
+     public K Info {get; set;}	    	// informação a guardar
+     public NodoD<K> Next {get; set;}	// apontador para o nodo seguinte
+     public NodoD<K> Prev {get; set;}	// apontador para o nodo anterior
+	 
+	 // construtor...
+	 public NodoD(K info)
+	  {
+	   this.Info=info;
+	   this.Next=null;
+	   this.Prev=null;
+	  } 
+	} // fim da classe...
+}
